c930566e4f147359fe192733e13119f3128f5704
bslewis2 <bslewis2@ncsu.edu>
2022-11-26 14:01:21 -0500

    added defaults and updated name in Duck

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ import {makeDuck as md, duckfaults as ud} from "./functions/ducker";
 import { Any as ANY } from "./functions/Any";
 
 export const makeDuck = md;
-export const updateDefaults = ud;
+export const duckfaults = ud;
 export const Any = ANY;
 
+export const isString = duckfaults(makeDuck(String), {
+    message: "Not a string",
+});
+
+export const isNumber = duckfaults(makeDuck(Number), {
+    message: "Not a number",
+});
+
+export const isObject = duckfaults(makeDuck(Object), {
+    message: "Not a object",
+});
+
+export const isBoolean = duckfaults(makeDuck(Boolean), {
+    message: "Not a boolean",
+});
+
+export const isArray = duckfaults(makeDuck(Array), {
+    message: "Not an array",
+})
+
 const Duck = {
-    updateDefaults,
+    duckfaults,
     makeDuck,
     Any,
 }
