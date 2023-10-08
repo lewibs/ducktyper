b0aa93a2647f9353cbe7c2ebfb064af1b4c1d8a9
lewibs <benjaminsl2000@gmail.com>
2023-10-08 00:55:09 -0400

    updated isClass

--- src/functions/is/isClass.ts
+++ src/functions/is/isClass.ts
@@ -1,39 +1,35 @@
-import isDuck from "./isDuck";
+// Import isDuck function from your module
+var isDuck = require("./isDuck");
 
-//taken from https://github.com/nof1000/isclass/blob/master/index.js
-//this is likely to not work in anything other then an ES6 environment might need to redo this
+// Define isClass function
 export default function isClass(cls) {
-  //saves time. no point in checking if it fails basics
+  // Check if cls is a function and has a prototype
   if (typeof cls === 'function' && cls.prototype) {
-    //really odd edgecase that can be found in "test any"
-    // if (isDuck(cls)) {
-    //   return false;
-    // }
-
+    // Attempt to detect if it's a class
     try {
-      //if it has the name class requires ES6
-      if (cls.toString().includes("class")) {
+      // Check if it has the name "class" in its string representation
+      if (/class/.test(cls.toString())) {
         return true;
       }
 
-      //this should throw if its a class requires ES6
+      // Attempt to instantiate it without 'new', which should throw an error for classes
       try {
         cls();
-      } catch(e: any) {
-        if (e.message === "Uncaught TypeError: class constructors must be invoked with 'new'") {
+      } catch(e) {
+        if (e instanceof TypeError && e.message.indexOf("class constructors must be invoked with 'new'") !== -1) {
           return true;
         }
       }
 
-      //last ditch attempt at hoping the user follows best practice is using older then ES6
-      if (cls.name.charAt(0) === cls.name.charAt(0).toUpperCase()) {
+      // Check if the name starts with an uppercase letter (assumes class naming convention)
+      if (/^[A-Z].*$/.test(cls.name)) {
         return true;
       }
     } catch (e) {
-      //if any of these throw its safe to assume its not a class
+      // If any of the checks throw an error, assume it's not a class
       return false;
     }
   }
-  //if all checks fail assume false
+  // If all checks fail, assume it's not a class
   return false;
 }
